995d34ac3d116571b4094aadc36113d8d5ee9414
Omar Duadu <a00315051@student.tus.ie>
2024-01-24 12:47:19 +0000

    Commit from Omar

--- src/main/java/com/tus/cipher/controllers/LoginService.java
+++ src/main/java/com/tus/cipher/controllers/LoginService.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginService {
     @GetMapping("/login")
     public void SystemAdminHomepage(@RequestBody LoginRequest loginDetail) {
-    	
+	// Testing login.js functionality    	
         System.out.println("Login endpoint accessed: " + loginDetail.getUsername() + ", pass: " loginDetail.getPassword());
         throw new NotYetImplementedException();
     }
