ba3bb07281da80f23ec5586b0b36a6b76a411767
Adijat Buraimoh <A00290545@student.tus.ie>
2024-01-24 13:51:35 +0000

    Login Service Update

--- src/main/java/com/tus/cipher/controllers/LoginService.java
+++ src/main/java/com/tus/cipher/controllers/LoginService.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tus.cipher.dto.LoginRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 public class LoginService {
     @GetMapping("/login")
-    public void SystemAdminHomepage(@RequestBody LoginRequest loginDetail) {
-	// Testing login.js functionality    	
-        System.out.println("Login endpoint accessed: " + loginDetail.getUsername() + ", pass: " loginDetail.getPassword());
+    public void login(@RequestBody LoginRequest loginDetail) {
+        System.out.println("Login endpoint accessed: " + loginDetail.getUsername() + ", pass: " + loginDetail.getPassword());
+    
         throw new NotYetImplementedException();
     }
 }
