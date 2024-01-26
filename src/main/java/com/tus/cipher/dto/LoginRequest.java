7e62a8a34f9d4837212e0072340420fdf9ce6369
Adijat Buraimoh <A00290545@student.tus.ie>
2024-01-26 11:46:49 +0000

    Update to LoginRequest

--- src/main/java/com/tus/cipher/dto/LoginRequest.java
+++ src/main/java/com/tus/cipher/dto/LoginRequest.java
@@ -3,9 +3,9 @@ package com.tus.cipher.dto;
 public class LoginRequest {
     private String username;
     private String password;
-
+    
     public LoginRequest() {
-
+    	
     }
 
 	public LoginRequest(String username, String password) {
@@ -29,6 +29,6 @@ public class LoginRequest {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
+    
+    
 }
