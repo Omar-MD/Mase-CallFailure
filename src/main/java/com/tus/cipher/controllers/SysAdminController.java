061ec66c62e5e3fe901e61ca8e963dc50c32c337
Omar Duadu <a00315051@student.tus.ie>
2024-01-30 02:33:26 +0000

    Added filename parameter to endpoint

--- src/main/java/com/tus/cipher/controllers/SysAdminController.java
+++ src/main/java/com/tus/cipher/controllers/SysAdminController.java
@@ -2,12 +2,16 @@ package com.tus.cipher.controllers;
 
 import java.io.IOException;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tus.cipher.dto.ImportRequest;
 import com.tus.cipher.services.ImportService;
 
 @RestController
@@ -17,11 +21,11 @@ public class SysAdminController {
 	@Autowired
 	ImportService importService;
 
+	//TODO: Clean up & improve error thrown
 	@PostMapping("/import")
-	public ResponseEntity<String> importData() throws IOException {
-		System.out.println("SysAdminController::importData");
+	public ResponseEntity<String> importData(@Valid @RequestBody ImportRequest importRequest) throws IOException {
 
-		importService.importFile("TUSGroupProject_SampleDataset.xls");
+		importService.importFile(importRequest.getFilename());
 		return ResponseEntity.ok("Import process completed");
 	}
 }
