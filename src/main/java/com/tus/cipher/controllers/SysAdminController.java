05e69b85428ce24124a8004419aad9119676ab49
Shane Neary <a00303376@student.tus.ie>
2024-01-30 11:11:45 +0000

    moved code related to the System Admin to it controller file

--- src/main/java/com/tus/cipher/controllers/SysAdminController.java
+++ src/main/java/com/tus/cipher/controllers/SysAdminController.java
@@ -2,15 +2,27 @@ package com.tus.cipher.controllers;
 
 import java.io.IOException;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tus.cipher.dao.AccountRepository;
+import com.tus.cipher.dto.accounts.Account;
+import com.tus.cipher.dto.accounts.AccountFactory;
 import com.tus.cipher.services.ImportService;
 
 @RestController
 public class SysAdminController {
 
 	private static final String FILE_NAME = "TUSGroupProject_SampleDataset.xls";
+
+	@Autowired
+    AccountRepository accountRepository;
 
 	@PostMapping("/import")
 	public void importData() {
@@ -23,4 +35,11 @@ public class SysAdminController {
 		}
 
 	}
+
+	@PostMapping("/accounts")
+    public ResponseEntity<Account> addAccount(@Valid @RequestBody Account account) {
+        account = AccountFactory.createAccount(account);
+        Account savedAccoount = accountRepository.save(account);
+        return ResponseEntity.status(HttpStatus.OK).body(savedAccoount);
+    }
 }
