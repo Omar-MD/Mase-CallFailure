af13969109aae925c63f50ceffa3a0099f6acf24
Omar Duadu <a00315051@student.tus.ie>
2024-02-20 22:00:36 +0000

    added searchable box

--- src/main/resources/static/js/query-modelFailureTypesCount.js
+++ src/main/resources/static/js/query-modelFailureTypesCount.js
@@ -2,7 +2,14 @@
 const addModelDropdown = function() {
     let model_dropdown = $("#modelFailureTypesCount-dropdown");
     model_dropdown.empty()
-    
+
+    model_dropdown.select2({
+        placeholder: "Begin typing Model to search..",
+        allowClear: true,
+        width: '100%',
+        minimumInputLength: 0
+    });
+
     $.ajax({
         type: 'GET',
         url: rootUrl + "/query/model-failures",
@@ -12,7 +19,7 @@ const addModelDropdown = function() {
             console.log(res);
             if (res.status == "Success") {
                 res.data.forEach(model => {
-                    model_dropdown.append("<option value="+model+">"+model+"</option>");
+                    model_dropdown.append("<option value=" + model + ">" + model + "</option>");
                 });
             } else {
                 console.log("Error:", res.error);
@@ -23,6 +30,7 @@ const addModelDropdown = function() {
         }
     });
 }
+
 const getModelFailureTypesWithCount = function() {
 
     let model = $("#modelFailureTypesCount-dropdown").val();
@@ -47,7 +55,7 @@ const getModelFailureTypesWithCount = function() {
         }
     });
 };
- 
+
 function updateModelTable(data) {
     $('#modelFailureTypesCount-datatable-body').empty();
     data.forEach(function(item) {
@@ -57,11 +65,11 @@ function updateModelTable(data) {
             <td>${item.failureCount}</td>
         </tr>`);
     });
-    
-    if(datatable) {
+
+    if (datatable) {
         datatable.destroy();
     }
-    
+
     datatable = $("#modelFailureTypesCount-datatable").DataTable({
         "sScrollY": "50vh",
         "bScrollCollapse": true
