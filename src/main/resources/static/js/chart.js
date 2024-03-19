06e2b74dd1a6bb9fad22ab27a8ba96ff7974ab0d
Shane Neary <a00303376@student.tus.ie>
2024-03-19 15:14:08 +0000

    Updated charts

--- src/main/resources/static/js/chart.js
+++ src/main/resources/static/js/chart.js
@@ -12,15 +12,24 @@ const saveChartToStack = function(modalName) {
 
 const addChart = function(chartData) {
     const modalName = chartData.modalName;
+    resetModalData(modalName);
+    $('#' + modalName + '-backButton').hide();
+    addModal(chartData.whereToAdd, modalName);
+    renderChart(modalName, chartData.title, chartData.chartDetails, chartData.clickHandler);
+    modalData[modalName].currentChart = chartData;
+}
+
+// Reset modalData
+const resetModalData = function(modalName) {
     if (!modalData[modalName]) {
         modalData[modalName] = {
             chartStack: [],
             currentChart: null
         };
+    } else {
+        modalData[modalName].chartStack.length = 0;
+        modalData[modalName].currentChart = null;
     }
-    addModal(chartData.whereToAdd, modalName);
-    renderChart(modalName, chartData.title, chartData.chartDetails, chartData.clickHandler);
-    modalData[modalName].currentChart = chartData;
 }
 
 // Creates Modal
@@ -68,6 +77,8 @@ const renderChart = function(modalName, title, chartDetails, clickHandler) {
     const ctx = canvas.getContext('2d');
     const mergedOptions = $.extend(true, {}, defaultChartOptions, chartDetails.options);
 
+    sortData(chartDetails);
+
     if (canvas.chartInstance) {
         canvas.chartInstance.config.data = chartDetails.data;
         canvas.chartInstance.config.options = mergedOptions;
@@ -85,6 +96,21 @@ const renderChart = function(modalName, title, chartDetails, clickHandler) {
         $(canvas).off('click');
     }
 };
+
+const sortData = function(chartDetails) {
+    let xData = chartDetails.data.datasets[0].data;
+    let yData = chartDetails.data.labels;
+
+    let indices = Array.from(xData.keys());
+    indices.sort((a, b) => xData[b] - xData[a]);
+
+    // Use the sorted indices to rearrange both lists
+    xData = indices.map(index => xData[index]);
+    yData = indices.map(index => yData[index]);
+
+    chartDetails.data.datasets[0].data = xData;
+    chartDetails.data.labels = yData;
+}
 
 const handleDrillDown = function(chartData) {
     const modalName = chartData.modalName;
